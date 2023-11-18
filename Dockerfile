963f3da602344c98250ec369764f5c8cad4fb7d2
Willem Houm <willem.houm@golem.ai>
2023-11-18 02:44:14 +0100

    init application

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# Use an official Python runtime as a parent image
+FROM python:3.12.0-slim
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory and file contents into the container at /app
+COPY ./citation-api/ ./.env ./requirements.txt /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --trusted-host pypi.python.org -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 80
+
+# Define environment variable
+# ENV APP_ENV=prod
+
+# Run app.py when the container launches
+CMD ["python", "/app/api.py"]
---
+++ citation-api/api.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from the .env file
+load_dotenv()
+
+app = Flask(__name__)
+
+# Check the value of APP_ENV to enable or disable debugging
+if os.getenv('APP_ENV') == 'dev':
+    app.debug = True
+
+# Get the port number from the APP_PORT environment variable or use 5000 as a default
+port = int(os.getenv('APP_PORT', 5000))
+
+# Define your API routes and functions here
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Add more routes and functions as needed
+
+if __name__ == '__main__':
+    # Run the application on the specified port
+    app.run(host='0.0.0.0', port=port)
